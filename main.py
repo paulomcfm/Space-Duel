00c2ed19641a47e2b09cd354913734504d40d90a
Paulo da Motta <paulomcfm@gmail.com>
2022-04-15 11:35:30 -0300

    Movimentação e Animação
    Modificando como o NPC se movimenta e alterando a sprite do player e NPC conforme a direção.

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ bgi = pygame.image.load("bg.png")
 def bg():
     screen.blit(bgi, (0, 0))
 
-
 # Jogador
 playeri = pygame.image.load('player.png')
 playeriright = pygame.image.load('playerRight.png')
@@ -24,41 +23,38 @@ playerileft = pygame.image.load('playerLeft.png')
 playerX = 25
 playerY = 350
 playerY_change = 0
+mov = 1
 
 
-def player(x,y):
-    screen.blit(playeri, (x, y))
+def player(x,y, mov):
+    if mov == 1:
+        screen.blit(playeri, (x, y))
+    elif mov == 2:
+        screen.blit(playeriright, (x, y))
+    elif mov == 3:
+        screen.blit(playerileft, (x, y))
 
 
-# 2 Jogar
-player2i = pygame.image.load('player2.png')
-player2iright = pygame.image.load('player2Right.png')
-player2ileft = pygame.image.load('player2Left.png')
-player2X = 900
-player2Y = 350
-player2Y_change = 0
+# NPC
+npci = pygame.image.load('player2.png')
+npciright = pygame.image.load('player2Right.png')
+npcileft = pygame.image.load('player2Left.png')
+npcX = 900
+npcY = 350
+npcY_change = 0
+movn = 1
 
-def player2(x,y):
-    pressed = 1
-    if pressed == 1:
-        screen.blit(player2i, (x, y))
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_DOWN:
-                screen.blit(player2ileft, (x, y))
-                pressed = 0
-        if event.key == pygame.K_UP:
-                screen.blit(player2iright, (x, y))
-                pressed = 0
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_DOWN or pygame.K_DOWN:
-            pressed =1
-
-
-
-
+def npc(x,y, movn):
+    if movn == 1:
+        screen.blit(npci, (x, y))
+    elif movn == 3:
+        screen.blit(npciright, (x, y))
+    elif movn == 2:
+        screen.blit(npcileft, (x, y))
 
 
 # Loop do Jogo
+
 rodando = True
 while rodando:
 
@@ -68,21 +64,34 @@ while rodando:
         if event.type == pygame.QUIT:
             rodando = False
 
+        # Movimentação do Player
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                playerY_change = 5
-                player2Y_change = 3
+                playerY_change = 6
+                mov = 2
             if event.key == pygame.K_UP:
                 playerY_change = -5
-                player2Y_change = -3
+                mov = 3
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN or pygame.K_DOWN:
                 playerY_change = 0
-                player2Y_change = 0
+                mov = 1
 
-    player2Y += player2Y_change
+    # Movimentação do NPC
+    if npcY < playerY and npcY != playerY:
+        npcY_change = 3
+        movn = 2
+    elif npcY > playerY and npcY != playerY:
+        npcY_change = -3
+        movn = 3
+    elif npcY == playerY:
+        npcY_change = 0
+        movn = 1
+
+    # Mudando Y do Player e NPC e Renderizando
+    npcY += npcY_change
     playerY += playerY_change
-    player(playerX,playerY)
-    player2(player2X, player2Y)
+    player(playerX, playerY, mov)
+    npc(npcX, npcY, movn)
 
     pygame.display.update()
